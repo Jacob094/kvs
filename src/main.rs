f613714680756725a33d4aae32b5a707f32f15fd
Jacob Ogle <jacbcodes@gmail.com>
2024-05-09 19:54:12 -0400

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// TODO: Remove once core development is done
+#![allow(unused_variables, unused_imports)]
+
+use crate::prelude::*;
+
+mod utils;
+mod prelude;
+
+fn main() -> Result<(), ()> {
+    println!("🦀 KVS... A simple key value store written in Rust");
+
+
+
+    Ok(())
 }
